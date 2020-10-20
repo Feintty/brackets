96d2ae5c3927775f2ad5fb561f1c2ea359fb8e9a
Feintty <makhmutoff.ildar2011@yandex.ru>
2020-10-20 21:11:41 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map(element => element.join(""));
+    regStr = ""
+    bracketsConfig.forEach(element => {
+		if (isNaN(Number(element))) {
+            regStr += '\\' + element[0] + '\\' + element[1] + "|";
+        } else {
+            regStr += element[0] + element[1] + "|";
+        }
+    })
+    reg = new RegExp(regStr.slice(0, -1));
+    while (reg.test(str)) {
+        str = str.replace(reg, '');
+    }
+    if (str == "") {
+        return true;
+    } else {
+        return false;
+    }
 }
